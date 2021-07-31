ea12ddbc411169c2d60d0bbfa73fc8e9e8940ac3
FranekWadas <franiowadas@gmail.com>
2021-07-31 21:33:24 +0200

    add a setup command

---
+++ commands/setup.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    "name": "setup",
+    "description": "Using that command you can setup bot to your discord server!",
+
+    execute(message, args, client) {
+
+        
+
+    }
+
+}
